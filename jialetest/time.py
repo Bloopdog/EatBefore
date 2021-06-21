452c9e019ec204e1f5c326de07e68d2e0c598d1e
Bloopdog <yc0708@hotmail.com>
2021-06-21 17:28:11 +0800

    Delete jialetest directory

--- jialetest/time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def get_curr_time():
-    # expected format: '2020-06-12 02:14:58'
-    x = datetime.datetime.now()
-    return (x.strftime("%Y-%m-%d %H:%M:%S"))
-
-print (get_curr_time())
